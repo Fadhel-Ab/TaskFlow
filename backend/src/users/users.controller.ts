47f478b2fc74a353b60a94b91aa9735d8b52836c
Fadhel <fadhel@tutamail.com>
2026-07-17 10:17:04 +0300

    Connect users controller with users service using dependency injection

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
   @Get()
   getUsers() {
-    return 'Getting all users';
+    return this.userService.getUsers();
   }
 }
