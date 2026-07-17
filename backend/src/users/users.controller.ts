a67df53379713f4e850002c6d2e893c6bed90436
Fadhel <fadhel@tutamail.com>
2026-07-17 11:31:18 +0300

    Add users CRUD endpoints with DTO validation

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -7,5 +8,13 @@ export class UsersController {
   @Get()
   getUsers() {
     return this.userService.getUsers();
+  }
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return this.userService.getUserById(Number(id));
+  }
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto.name);
   }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,7 +2,28 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Ahmed',
+    },
+    {
+      id: 2,
+      name: 'Ali',
+    },
+  ];
   getUsers() {
-    return 'users from services';
+    return this.users;
+  }
+  getUserById(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+  createUser(name: string) {
+    const newUser = {
+      id: this.users.length + 1,
+      name,
+    };
+    this.users.push(newUser);
+    return newUser;
   }
 }
