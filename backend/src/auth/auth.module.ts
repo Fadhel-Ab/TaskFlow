0abb64532d4c80bd743d956ea8c6a6c6a090c958
Fadhel <fadhel@tutamail.com>
2026-07-18 07:13:43 +0300

    add role guard for route protection

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,10 +3,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { RolesGuard } from './roles.guard';
 
 @Module({
   imports: [
     UsersModule,
+    PassportModule,
     JwtModule.register({
       secret: 'secretKey',
       signOptions: {
@@ -15,6 +19,6 @@ import { AuthService } from './auth.service';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, RolesGuard],
 })
 export class AuthModule {}
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,11 +1,17 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Roles } from '../auth/roles.decorator';
+import { RolesGuard } from '../auth/roles.guard';
+
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
   @Get()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('MANAGER')
   getUsers() {
     return this.userService.getUsers();
   }
